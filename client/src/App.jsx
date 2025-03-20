61bd4be442909ef3c7284f4cf689291450ae1665
Marcos Ilgi <marcos.ilgi@gmail.com>
2025-03-20 14:06:08 +0100

    Update title in index.html and integrate Logout and UserProfile components in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import Login from "./supabase/login"
+import Logout from "./supabase/logout"
+import UserProfile from "./supabase/userProfile"
 
 function App() {
 
   return (
     <>
       <Login />
+      <Logout />
+      <UserProfile />
     </>
   )
 }
