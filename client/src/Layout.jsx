a54f25cc6a80ceb840c761bddc2b39c4f78d0a82
Marcos Ilgi <marcos.ilgi@gmail.com>
2025-03-21 14:02:43 +0100

    Add Vercel Analytics integration and configuration files

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,15 +1,18 @@
+import { Analytics } from "@vercel/analytics/react"
 import Header from './components/Header';
 import FooterMenu from './components/FooterMenu';
 import CheckoutButton from './components/CheckoutButton';
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main>
+        {children}
         <CheckoutButton />
       </main>
       <FooterMenu />
+      <Analytics />
     </>
   );
 }
